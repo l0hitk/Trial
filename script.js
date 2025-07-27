0a78da10a507d174749acfc8729d5a04c634cff4
Lohit K <127608339+l0hitk@users.noreply.github.com>
2025-07-28 00:44:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -75,11 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 { title: "AI, ML & Insights", links: [ { text: "Data Science", href: "/services/data-science.html" }, { text: "AI Engineering", href: "/services/ai-engineering.html" }, { text: "ML Products & Platforms", href: "/services/ml-products-platforms.html" }, { text: "Business Intelligence", href: "/services/business-intelligence.html" }, { text: "MLOps", href: "/services/mlops.html" } ] }
             ],
             bottom: {
-                copyright: `© ${new Date().getFullYear()} DSCRUTINY Inc. All Rights Reserved.`,
-                socials: [
-                    { name: "LinkedIn", href: "#", icon: '<path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle>' },
-                    { name: "Twitter", href: "#", icon: '<path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>' }
-                ]
+                copyright: `© ${new Date().getFullYear()} DSCRUTINY Inc. All Rights Reserved.`
             }
         };
 
